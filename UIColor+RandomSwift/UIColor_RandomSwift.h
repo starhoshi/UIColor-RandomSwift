c61efd9dca123a1e2f90f22f4571bf85f31a7218
Kensuke Hoshikawa <kensuke-hoshikawa@cookpad.com>
2018-03-03 22:05:04 +0900

    Initial Commit

---
+++ UIColor+RandomSwift/UIColor_RandomSwift.h
@@ -0,0 +1,19 @@
+//
+//  UIColor_RandomSwift.h
+//  UIColor+RandomSwift
+//
+//  Created by kensuke-hoshikawa on 2018/03/03.
+//  Copyright © 2018年 star__hoshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIColor_RandomSwift.
+FOUNDATION_EXPORT double UIColor_RandomSwiftVersionNumber;
+
+//! Project version string for UIColor_RandomSwift.
+FOUNDATION_EXPORT const unsigned char UIColor_RandomSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIColor_RandomSwift/PublicHeader.h>
+
+
---
+++ UIColor+RandomSwiftTests/UIColor_RandomSwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  UIColor_RandomSwiftTests.swift
+//  UIColor+RandomSwiftTests
+//
+//  Created by kensuke-hoshikawa on 2018/03/03.
+//  Copyright © 2018年 star__hoshi. All rights reserved.
+//
+
+import XCTest
+@testable import UIColor_RandomSwift
+
+class UIColor_RandomSwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
